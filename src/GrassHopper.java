40e78afcd653655f7d08b3ffa897ffded6260a8f
Igor Ryabchuk <igor.ryabchuk@tesco.com>
2018-09-06 17:51:08 +0100

    Add first passing test

---
+++ src/GrassHopper.java
@@ -0,0 +1,7 @@
+public class GrassHopper {
+
+  public static int summation(int n) {
+
+    return 3;
+  }
+}
---
+++ src/GrassHopperTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrassHopperTest {
+
+  @Test
+  @DisplayName("Should return 3 when give 2 as initial value")
+  public void returnsThreeforTwo () {
+    assertEquals (3,GrassHopper.summation (2));
+  }
+
+}
